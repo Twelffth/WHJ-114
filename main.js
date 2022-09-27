336f1a238444ee90f9e4ef0333fd3815101da052
Twelffth <96769395+Twelffth@users.noreply.github.com>
2022-09-27 20:58:06 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() {
+
+
+}
+
+function setup() {
+    canvas = createCanvas(780, 480);
+    canvas.center();
+    video = createCapture(VIDEO);
+
+}
+function draw() {
+
+
+}
+
+function onClick() {
+
+
+}
